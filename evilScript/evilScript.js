91d3b3b92ceff749f81641f67955e10d18060bac
sinou <srinivasane.arav@gmail.com>
2014-10-14 00:08:21 -0400

    work in progress - works, but hacky

---
+++ evilScript/evilScript.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+window.open('','_self').close();//closes every tab that opens, chrome only allows scripts to close the tabs they open
+								// 
+ setTimeout(function(){
+  if ($(location).attr('href').indexOf("free_points") >= 0){
+	$("#id_rh").trigger("click");
+	$("body").prepend("<div>"+
+	"<button id = 'evil_button' type='button' >Get Points!</button>" +"</div>")
+  }
+   $("#evil_button").click(function(){
+		var url = "http://www.bing.com/search?q=";
+		//window.location = url + Math.random();
+		for(i = 0; i < 90; i++){
+			var win = window.open(url + Math.random(), '_blank');
+			win.focus();
+		}
+		
+  });
+}, 2000);
+ 
+  
+});
+
+
