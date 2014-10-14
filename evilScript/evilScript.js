7a94f1e79902e7619940985d1ea0a09988e07ecd
sinou <srinivasane.arav@gmail.com>
2014-10-14 11:44:07 -0400

    latest

--- evilScript/evilScript.js
+++ evilScript/evilScript.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
- if ($(location).attr('href').indexOf("free_points") >= 0){
-window.open('','_self').close();//closes every tab that opens, chrome only allows scripts to close the tabs they open
-}						// 
- setTimeout(function(){
-  if ($(location).attr('href').indexOf("free_points") >= 0){
-	$("#id_rh").trigger("click");
-	$("body").prepend("<div>"+
-	"<button id = 'evil_button' type='button' >Get Points!</button>" +"</div>")
-  }
-   $("#evil_button").click(function(){
+	if ($(location).attr('href').indexOf("&free_points") >= 0){
+		window.open('','_self').close();//closes every tab that opens, chrome only allows scripts to close the tabs they open
+	}						// 
+
+	  if ($(location).attr('href').indexOf("?free_points") >= 0){
+		//$("#id_rh").trigger("click");
+		$("body").prepend("<div>"+
+		"<button id = 'evil_button' type='button' >Get Points!</button>" +"</div>")
+	  }
+	  $("#evil_button").click(function(){
 		var url = "http://www.bing.com/search?q=";
 		//window.location = url + Math.random() ;
 		for(i = 0; i < 90; i++){
 			var win = window.open(url + Math.random() + "&free_points=true", '_blank');
 			win.focus();
 		}
-		
-  });
-}, 2000);
- 
-  
+		window.open('','_self').close();
+	  });
+	$("#evil_button").click();
 });
+
+
